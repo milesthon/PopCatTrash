4c0ff0b7fb1ea6d5df136c7e6a9033b028928af4
Michael Milesthon <103425609+milesthon@users.noreply.github.com>
2023-05-25 14:55:34 +0700

    Add files via upload

--- apply.bat
+++ apply.bat
@@ -1,15 +1,16 @@
-@echo off
+:: @echo off
 CHCP 65001>NUL
 
 echo.&echo Wait..
-md "C:\Users\%UserName%\AppData\Local\PopCatTrushIco"                           2>nul >nul
-copy "%~dp0empty.ico" "C:\Users\%UserName%\AppData\Local\PopCatTrush\empty.ico" 2>nul >nul
-copy "%~dp0full.ico" "C:\Users\%UserName%\AppData\Local\PopCatTrush\full.ico"   2>nul >nul
+md "C:\Users\%UserName%\AppData\Local\PopCatTrushIco"                              2>nul >nul
+copy "%~dp0empty.ico" "C:\Users\%UserName%\AppData\Local\PopCatTrushIco\empty.ico" 2>nul >nul
+copy "%~dp0full.ico" "C:\Users\%UserName%\AppData\Local\PopCatTrushIco\full.ico"   2>nul >nul
 
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V empty /D "%USERPROFILE%\AppData\Local\PopCatTrush\empty.ico" /T REG_EXPAND_SZ /F 2>nul >nul
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V full  /D "%USERPROFILE%\AppData\Local\PopCatTrush\full.ico"  /T REG_EXPAND_SZ /F 2>nul >nul
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V empty /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\empty.ico" /T REG_EXPAND_SZ /F 2>nul >nul
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V full  /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\full.ico"  /T REG_EXPAND_SZ /F 2>nul >nul
 
 ie4uinit.exe -show                                                                                     2>nul >nul
+pause
 PowerShell @^(^(New-Object -com shell.application^).Windows^(^)^).Document.Folder.Self.Path >> "%temp%\paths.txt"
 TaskKill /F /IM Explorer.exe                                                                           2>nul >nul
 Start Explorer.exe                                                                                     2>nul >nul
