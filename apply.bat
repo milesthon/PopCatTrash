d5ee38e83889720a59b95bbc0be4c6d5a2ee52be
Michael Milesthon <103425609+milesthon@users.noreply.github.com>
2023-06-01 09:59:41 +0700

    Add files via upload

--- apply.bat
+++ apply.bat
@@ -9,13 +9,13 @@ copy "%~dp0full.ico" "C:\Users\%UserName%\AppData\Local\PopCatTrushIco\full.ico"
 for /f "tokens=2" %%a in ('reg query "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /ve') do set DefaultValue=%%a
 
 if "%DefaultValue%"=="-45" (
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V empty /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\empty.ico" /T REG_EXPAND_SZ /F 2>nul >nul
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V full  /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\full.ico"  /T REG_EXPAND_SZ /F 2>nul >nul
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /VE      /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\full.ico"  /T REG_EXPAND_SZ /F 2>nul >nul
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V empty /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\empty.ico, 0" /T REG_EXPAND_SZ /F 2>nul >nul
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V full  /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\full.ico, 0"  /T REG_EXPAND_SZ /F 2>nul >nul
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /VE      /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\full.ico, 0"  /T REG_EXPAND_SZ /F 2>nul >nul
 ) else (
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V empty /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\empty.ico" /T REG_EXPAND_SZ /F 2>nul >nul
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V full  /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\full.ico"  /T REG_EXPAND_SZ /F 2>nul >nul
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /VE      /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\empty.ico" /T REG_EXPAND_SZ /F 2>nul >nul
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V empty /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\empty.ico, 0" /T REG_EXPAND_SZ /F 2>nul >nul
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /V full  /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\full.ico, 0"  /T REG_EXPAND_SZ /F 2>nul >nul
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" /VE      /D "%USERPROFILE%\AppData\Local\PopCatTrushIco\empty.ico, 0" /T REG_EXPAND_SZ /F 2>nul >nul
 )
 
 ie4uinit.exe -show 2>nul >nul
